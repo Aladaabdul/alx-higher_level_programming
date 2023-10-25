d24ecd925d32b7fcfddda758903928835df30878
aladarahman <aladarahman@gmail.com>
2023-10-25 13:53:40 +0100

    Coordinates of a square

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -44,7 +44,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.__size = size
+            self.__size = value
 
     @property
     def position(self):
@@ -69,7 +69,7 @@ class Square:
         if type(value) is not tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         else:
-            return self.__position
+            self.__position = value
 
     def area(self):
         """Initialize area
