69d31bb3ff21cccddd66bc4614aca8e0b62e7b06
aladarahman <aladarahman@gmail.com>
2023-10-30 17:16:50 +0100

    class Rectangle that defines a rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -19,7 +19,7 @@ class Rectangle:
         Return: None
         """
         self.__width = width
-        self.height = height
+        self.__height = height
 
     @property
     def width(self):
