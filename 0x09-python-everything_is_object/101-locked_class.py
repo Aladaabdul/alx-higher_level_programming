579ed8046b94e71abafd1aeed2ee38b96669842a
aladarahman <aladarahman@gmail.com>
2023-10-31 18:45:57 +0100

    101-locked_class.py

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+class LockedClass:
+    """ Defination of LockedClass
+
+    Attributes:
+    first_name: String
+    
+    Return: first_name
+    """
+    __slots__ = ["first_name"]
+
+    def __init__(self):
+        """Class Initialization
+        
+        Args:
+        FisrtName: string
+
+        Return: None
+        """
+        self.first_name = "first_name"
