e4d648a33e754587b0078279ea8ccbd4b2c093f4
aladarahman <aladarahman@gmail.com>
2023-11-08 16:34:26 +0100

    a class Student that defines a student

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Student class defination
+
+
+"""
+
+
+class Student():
+    """Student class
+
+    Attributes:
+    first_name: String
+    last_name: String
+    age: Int
+
+    """
+    def __init__(self, first_name, last_name, age):
+        """class Initialization
+
+        Args:
+        first_name
+        last_name
+        age
+
+        Returns: None
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Function defination
+
+        """
+        if attrs is None:
+            return self.__dict__
+        else:
+            return {attr: getattr(self, attr) for
+                    attr in attrs if hasattr(self, attr)}
+
+    def  reload_from_json(self, json):
+        """Function defination
+
+        """
+        for k, v in json.items():
+            setattr(self, k, v)
