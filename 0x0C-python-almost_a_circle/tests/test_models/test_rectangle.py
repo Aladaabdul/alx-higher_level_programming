d9d4be68d4c710ff6397edd5c24f6f894249dd00
aladarahman <aladarahman18@gmail.com>
2023-11-13 19:23:04 +0100

    tests directory

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Unittest for class rectangle"""
+
+import unittest
+import io
+import contextlib
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
+
+
+class TestRectangle(unittest.TestCase):
+    """Test cases for the Rectangle class."""
+
+    def setUp(self):
+        Base._Base__nb_objects = 0
+
+    def test_2_0(self):
+        """Checking for id."""
+
+        r0 = Rectangle(1, 2)
+        self.assertEqual(r0.id, 1)
+        r1 = Rectangle(2, 3)
+        self.assertEqual(r1.id, 2)
+        r2 = Rectangle(3, 4)
+        self.assertEqual(r2.id, 3)
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+        r4 = Rectangle(10, 2, 4, 5, 34)
+        self.assertEqual(r4.id, 34)
+        r5 = Rectangle(10, 2, 4, 5, -5)
+        self.assertEqual(r5.id, -5)
+        r6 = Rectangle(10, 2, 4, 5, 9)
+        self.assertEqual(r6.id, 9)
