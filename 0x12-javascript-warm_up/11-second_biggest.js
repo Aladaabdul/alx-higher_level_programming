fa01af6512ea169bf2a9699cbf55a8d11d03cf98
aladarahman <aladarahman18@gmail.com>
2024-01-09 14:10:54 +0100

    11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,15 +1,10 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2);
-let largest = args[0];
 
 if (args.length === 0 || args.length === 1) {
   console.log(0);
 } else {
-  for (let i = 1; i < args.length; i++) {
-    if (args[i] > largest) {
-      largest = args[i];
-    }
-  }
+  const largest = Math.max(...args.map(Number));
   console.log(largest);
 }
