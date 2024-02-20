3e974a8efc9c506e7b9de136f2512a789135821e
aladarahman <aladarahman18@gmail.com>
2024-02-20 13:54:53 +0100

    1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,8 @@ const fs = require('fs');
 const process = require('process');
 const args = process.argv.slice(2);
 
-fs.writeFile(args[0], args[1], 'utf8', err => {
+fs.writeFile(args[0], args[1], 'utf-8', err => {
   if (err) {
     console.log(err);
-    return;
   }
-  console.log('Successful');
 });
